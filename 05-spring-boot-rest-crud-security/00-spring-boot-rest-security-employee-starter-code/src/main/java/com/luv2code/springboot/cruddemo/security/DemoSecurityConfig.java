7f13cedff0143fe512918fc409f9db9db0fe9c8b
Nishith Gangajaliya <84803886+YoNishu@users.noreply.github.com>
2024-04-07 19:26:25 +0530

    Add files via upload

---
+++ 05-spring-boot-rest-crud-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+//        UserDetails bhavesh = User.builder()
+//                .username("bhavesh")
+//                .password("{noop}something123")
+//                .roles("EMPLOYEE")
+//                .build();
+//        UserDetails neha = User.builder()
+//                .username("neha")
+//                .password("{noop}something123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//        UserDetails noopur = User.builder()
+//                .username("noopur")
+//                .password("{noop}something123")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(bhavesh,neha,noopur);
+//    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager (DataSource dataSource) {
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "SELECT user_id, pw, active FROM members WHERE user_id=?"
+        );
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "SELECT user_id, role FROM roles WHERE user_id=?"
+        );
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+}
