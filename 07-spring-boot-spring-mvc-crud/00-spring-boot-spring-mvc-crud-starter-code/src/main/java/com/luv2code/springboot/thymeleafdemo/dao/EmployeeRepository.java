bad5e8e8e11fb1b5a5f1adb5f73456f6fd906e3c
Nishith Gangajaliya <84803886+YoNishu@users.noreply.github.com>
2024-04-08 23:38:24 +0530

    Add files via upload
    Full spring mvc crud app with UI using Thymeleaf.

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    //custom name, JPA will auto-magically create query behind the scenes
+    public List<Employee> findAllByOrderByFirstNameAsc();
+}
